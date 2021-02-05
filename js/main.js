648df3699b26365dbbc0bed2c753cfa9bcdbb240
krosh22 <Olga2018r@yandex.ru>
2021-02-06 00:53:54 +0300

    ggx

--- js/main.js
+++ js/main.js
@@ -1,58 +1,45 @@
-const flowersSlider = new Swiper('.flowers-slider', {
-    // Optional parameters
-
-    loop: true,
-    slidesPerView: 6,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+$ (document).ready(function (){
 
 
+    const flowersSlider = new Swiper('.flowers-slider', {
+// Optional parameters
 
-
-    breakpoints: {
-
-        320: {
-            slidesPerView: 2,
-        },
-        480: {
-            slidesPerView: 3,
+        loop: true,
+        slidesPerView: 6,
+// Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
 
-        992: {
-            slidesPerView: 6,
+        breakpoints: {
+
+            320: {
+                slidesPerView: 2,
+            },
+            480: {
+                slidesPerView: 3,
+            },
+
+            992: {
+                slidesPerView: 6,
+            }
         }
-    }
-});
+    });
 
+    const reviewsSlider = new Swiper('.reviews-slider', {
+// Optional parameters
 
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 100,
+// Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
 
-
-
-const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-
-    loop: true,
-    slidesPerView: 1,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-
-    },
-    breakpoints: {
-
-        320: {
-            slidesPerView: 1,
         },
-        480: {
-            slidesPerView: 1,
-        },
+    });
 
-        992: {
-            slidesPerView: 1,
-        }
-    }
+
 });
