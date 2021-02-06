8fcc07fbba5af76009daeb71bdb1a63b5fdf7798
krosh22 <Olga2018r@yandex.ru>
2021-02-06 18:15:42 +0300

    ggx

--- js/main.js
+++ js/main.js
@@ -43,3 +43,29 @@ $ (document).ready(function (){
 
 
 });
+
+$("#rev-1").on('click', function() {
+
+    $.fancybox.open([
+        {
+            src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+            opts : {
+                caption : 'First caption',
+                thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+            }
+        },
+        {
+            src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+            opts : {
+                caption : 'Second caption',
+                thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+            }
+        }
+    ], {
+        loop : true,
+        thumbs : {
+            autoStart : true
+        }
+    });
+
+});
